37c2f2e8d79476108ed5c4997c2bb36bce4072be
Abdullh1111 <abdullah4474032@gmail.com>
2025-04-19 01:57:15 +0600

    feat: add user data

---
+++ app/api/auth/userData/route.ts
@@ -0,0 +1,27 @@
+import { getUserFromToken } from "@/lib/JwtUser";
+import { handleAxiosError } from "@/utils/errorHandler";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { AxiosError } from 'axios';
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+    try {
+        const user = await getUserFromToken();
+        if (!user) {
+          return NextResponse.json({ message: "User not found" }, { status: 404 });
+        }
+        return NextResponse.json({ data: user, message: "Users fetched successfully", status: true });
+    } catch (error) {
+        const errorDetails = handleAxiosError(error as AxiosError);
+        return NextResponse.json(
+          { message: errorDetails.message, errors: errorDetails.errors },
+          {
+            status:
+              error instanceof AxiosError ? error.response?.status || 500 : 500,
+          }
+        );
+    }
+    
+}
